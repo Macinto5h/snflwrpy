cd01f02653b5d0a2020cf66deeaf7a440a130f0e
Macallan Camara <44030647+Macinto5h@users.noreply.github.com>
2022-09-11 17:00:56 -0400

    Add radix lsd sorter tests to repo

---
+++ tests/sorters/test_radix_lsd_sorter.py
@@ -0,0 +1,21 @@
+"""Test suite for radix_lsd_sorter module"""
+from vogel_sorter.sorters.radix_lsd_sorter import RadixLSDSorter
+from vogel_sorter.sorters.sort_change import SortChange
+from chancepy import Chance
+
+def test_radix_lsd_sorter_is_sorted_after_iterating_list_length_times_digit_length():
+    """Test radix lsd sorter is sorted after iterating next n squared times"""
+
+    unsorted_array = []
+    list_length = Chance.integer(2, 20)
+    digit_length = 3
+
+    for i in range(list_length):
+        unsorted_array.append(Chance.integer(100, 999))
+
+    sorter = RadixLSDSorter(unsorted_array)
+
+    for i in range(list_length ** digit_length):
+        sorter.next()
+
+    assert sorter.is_sorted()
