b1c385ab1f11a2e2a6b8d691fb647bc9bee96f5f
Macallan Camara <44030647+Macinto5h@users.noreply.github.com>
2022-09-18 21:19:33 -0400

    Rename vogel_cli to cli

---
+++ snflwr/cli.py
@@ -0,0 +1,23 @@
+"""
+This module has the command line interface for snflwr that allows a user to
+run the app with various options.
+"""
+import argparse
+from snflwr import __version__
+from snflwr import __description__
+from snflwr.sort_type import SortType
+from snflwr.main import Application
+
+def cli():
+    """Command line interface to run the snflwr application."""
+
+    parser = argparse.ArgumentParser(description=__description__)
+    parser.add_argument('-a', '--algorithm', choices=SortType.get_values(), dest='algorithm')
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+
+    args = parser.parse_args()
+
+    Application().start(args.algorithm)
+
+if __name__ == "__main__":
+    cli()
